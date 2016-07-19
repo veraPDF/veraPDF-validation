6ca2b2a8dfba67ad246f38bae3eaec2560bfb06f
Maksim Bezrukov <maksim.bezrukov@duallab.com>
2016-07-19 13:10:43 +0300

    Implementation of ICCProfile classes

---
+++ greenfield-validation-model/src/main/java/org/verapdf/model/impl/external/GFExternal.java
@@ -0,0 +1,15 @@
+package org.verapdf.model.impl.external;
+
+import org.verapdf.model.GenericModelObject;
+import org.verapdf.model.external.External;
+
+/**
+ * @author Maksim Bezrukov
+ */
+public class GFExternal extends GenericModelObject implements External {
+
+	protected GFExternal(String type) {
+		super(type);
+	}
+
+}
