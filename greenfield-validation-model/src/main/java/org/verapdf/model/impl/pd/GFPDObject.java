909578299f7118acfa8a195bda10cc739d808211
Timur Kamalov <timur.kamalov@duallab.com>
2016-07-28 15:30:02 +0300

    Added resources.

--- greenfield-validation-model/src/main/java/org/verapdf/model/impl/pd/GFPDObject.java
+++ greenfield-validation-model/src/main/java/org/verapdf/model/impl/pd/GFPDObject.java
@@ -2,6 +2,7 @@ package org.verapdf.model.impl.pd;
 
 import org.verapdf.model.GenericModelObject;
 import org.verapdf.model.pdlayer.PDObject;
+import org.verapdf.pd.PDContentStream;
 import org.verapdf.pd.PDDocument;
 
 /**
@@ -12,11 +13,17 @@ public class GFPDObject extends GenericModelObject implements PDObject {
 	public static final int MAX_NUMBER_OF_ELEMENTS = 1;
 
 	protected PDDocument document;
+	protected PDContentStream contentStream;
 	protected org.verapdf.pd.PDObject simplePDObject;
 
 	public GFPDObject(PDDocument document, final String type) {
 		super(type);
 		this.document = document;
+	}
+
+	public GFPDObject(PDContentStream contentStream, final String type) {
+		super(type);
+		this.contentStream = contentStream;
 	}
 
 	public GFPDObject(org.verapdf.pd.PDObject simplePDObject, final String type) {
