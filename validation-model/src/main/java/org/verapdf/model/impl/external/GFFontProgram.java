0d2092611c26551cc18ee2f82210de1b722e8ae5
shem-sergey <sergey.shemyakov@duallab.com>
2016-08-29 17:59:07 +0300

    getFontFile added to GFPDFont. Docs added.

--- validation-model/src/main/java/org/verapdf/model/impl/external/GFFontProgram.java
+++ validation-model/src/main/java/org/verapdf/model/impl/external/GFFontProgram.java
@@ -9,7 +9,18 @@ import org.verapdf.model.external.FontProgram;
  */
 public class GFFontProgram extends GFExternal implements FontProgram {
 
+    public static final String FONT_PROGRAM_TYPE = "FontProgram";
+
     protected GFFontProgram(String type) {
         super(type);
+    }
+
+    /**
+     * This constructor is in fact a dummy. Method getFontFile in PDFont must
+     * return FontProgram, and something is needed for fonts that are not True
+     * Type.
+     */
+    public GFFontProgram() {
+        super(FONT_PROGRAM_TYPE);
     }
 }
