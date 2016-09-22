302ffe5213a5bf7407ac6414730529108766d2b4
shem-sergey <sergey.shemyakov@duallab.com>
2016-09-22 12:08:06 +0300

    Created GFPDSignatureField.

---
+++ validation-model/src/main/java/org/verapdf/model/impl/pd/signature/GFPDSignatureField.java
@@ -0,0 +1,20 @@
+package org.verapdf.model.impl.pd.signature;
+
+import org.verapdf.model.impl.pd.GFPDFormField;
+import org.verapdf.model.pdlayer.PDSignature;
+
+/**
+ * @author Sergey Shemyakov
+ */
+public class GFPDSignatureField extends GFPDFormField implements PDSignature {
+
+    /**
+     * Type name for {@code PBoxPDSignatureField}
+     */
+    public static final String SIGNATURE_FIELD_TYPE = "PDSignatureField";
+
+    public static final String SIGNATURE_DICTIONARY = "V";
+
+
+
+}
