2ef7f3c163a1082039dc4f7990237d4b98fb6a2a
shem-sergey <sergey.shemyakov@duallab.com>
2016-09-23 11:18:26 +0300

    Finished PDSignatureField.

--- validation-model/src/main/java/org/verapdf/model/impl/pd/signature/GFPDSignatureField.java
+++ validation-model/src/main/java/org/verapdf/model/impl/pd/signature/GFPDSignatureField.java
@@ -1,12 +1,19 @@
 package org.verapdf.model.impl.pd.signature;
 
+import org.verapdf.cos.COSObject;
+import org.verapdf.model.baselayer.Object;
 import org.verapdf.model.impl.pd.GFPDFormField;
 import org.verapdf.model.pdlayer.PDSignature;
+import org.verapdf.model.pdlayer.PDSignatureField;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Sergey Shemyakov
  */
-public class GFPDSignatureField extends GFPDFormField implements PDSignature {
+public class GFPDSignatureField extends GFPDFormField implements PDSignatureField {
 
     /**
      * Type name for {@code PBoxPDSignatureField}
@@ -15,6 +22,34 @@ public class GFPDSignatureField extends GFPDFormField implements PDSignature {
 
     public static final String SIGNATURE_DICTIONARY = "V";
 
+    public GFPDSignatureField(org.verapdf.pd.form.PDSignatureField pdSignatureField) {
+        super(pdSignatureField, SIGNATURE_FIELD_TYPE);
+    }
 
+    @Override
+    public List<? extends Object> getLinkedObjects(String link) {
+        switch (link) {
+            case SIGNATURE_DICTIONARY:
+                return getSignatureDictionary();
+            default:
+                return super.getLinkedObjects(link);
+        }
+    }
 
+    /**
+     * Here we suppose that signature can be present in signature dictionary
+     * only indirectly. If it is not then we will get ClassCastException.
+     */
+    private List<PDSignature> getSignatureDictionary() {
+        org.verapdf.pd.PDSignature signature =
+                ((org.verapdf.pd.form.PDSignatureField) this.simplePDObject).getSignature();
+        if (signature != null) {
+            List<PDSignature> list = new ArrayList<>(MAX_NUMBER_OF_ELEMENTS);
+            COSObject signatureReference = ((org.verapdf.pd.form.PDSignatureField)
+                    this.simplePDObject).getSignatureReference();
+            list.add(new GFPDSignature(signature, document, signatureReference));
+            return Collections.unmodifiableList(list);
+        }
+        return Collections.emptyList();
+    }
 }
