a99511de4e2ba27d44a26dc60b512248de2dcd54
Carl Wilson <carl@openplanetsfoundation.org>
2016-10-27 12:14:38 +0100

    FEAT -Components - Foundries are the first component; and - Components simply report nice details.

--- validation-model/src/main/java/org/verapdf/pdfa/GreenfieldFoundry.java
+++ validation-model/src/main/java/org/verapdf/pdfa/GreenfieldFoundry.java
@@ -1,23 +1,32 @@
 package org.verapdf.pdfa;
 
 import java.io.InputStream;
+import java.net.URI;
 
+import org.verapdf.ReleaseDetails;
+import org.verapdf.component.ComponentDetails;
+import org.verapdf.component.Components;
 import org.verapdf.core.EncryptedPdfException;
 import org.verapdf.core.ModelParsingException;
 import org.verapdf.gf.model.GFModelParser;
-import org.verapdf.pdfa.MetadataFixer;
-import org.verapdf.pdfa.PDFParser;
-import org.verapdf.pdfa.VeraPDFFoundry;
 import org.verapdf.pdfa.flavours.PDFAFlavour;
 
 /**
  * @author Maksim Bezrukov
  */
 class GreenfieldFoundry extends AbstractFoundry {
+	private static final URI id = URI.create("http://foundries.verapdf.org#greenfield");
+	private static final ComponentDetails details = Components.detailsFromValues(id, "VeraPDF Greenfield Foundry", "0.1.0");
 	private static final VeraPDFFoundry instance = new GreenfieldFoundry();
+
 	private GreenfieldFoundry() {
+		super();
 	}
 
+	@Override
+	public ComponentDetails getDetails() {
+		return details;
+	}
 	/**
 	 * @see org.verapdf.pdfa.VeraPDFFoundry#newPdfParser(java.io.InputStream)
 	 */
@@ -27,7 +36,8 @@ class GreenfieldFoundry extends AbstractFoundry {
 	}
 
 	/**
-	 * @see org.verapdf.pdfa.VeraPDFFoundry#newPdfParser(java.io.InputStream, org.verapdf.pdfa.flavours.PDFAFlavour)
+	 * @see org.verapdf.pdfa.VeraPDFFoundry#newPdfParser(java.io.InputStream,
+	 *      org.verapdf.pdfa.flavours.PDFAFlavour)
 	 */
 	@Override
 	public PDFParser newPdfParser(InputStream pdfStream, PDFAFlavour flavour)
@@ -43,7 +53,7 @@ class GreenfieldFoundry extends AbstractFoundry {
 		// TODO: implement me with metadata fixer
 		return null;
 	}
-	
+
 	static VeraPDFFoundry getInstance() {
 		return instance;
 	}
