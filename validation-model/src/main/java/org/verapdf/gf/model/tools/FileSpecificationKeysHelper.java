9441f5bb3eb7feee572c17c87c3761be7f8cd977
Maksim Bezrukov <bezrukovm95@gmail.com>
2016-11-01 11:23:41 +0300

    Fixed cycles in AF keys evaluating

--- validation-model/src/main/java/org/verapdf/gf/model/tools/FileSpecificationKeysHelper.java
+++ validation-model/src/main/java/org/verapdf/gf/model/tools/FileSpecificationKeysHelper.java
@@ -1,28 +1,9 @@
 package org.verapdf.gf.model.tools;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.verapdf.as.ASAtom;
-import org.verapdf.cos.COSArray;
-import org.verapdf.cos.COSDocument;
-import org.verapdf.cos.COSKey;
-import org.verapdf.cos.COSObjType;
-import org.verapdf.cos.COSObject;
+import org.verapdf.cos.*;
 import org.verapdf.gf.model.impl.containers.StaticContainers;
-import org.verapdf.pd.PDAnnotation;
-import org.verapdf.pd.PDAppearanceEntry;
-import org.verapdf.pd.PDAppearanceStream;
-import org.verapdf.pd.PDCatalog;
-import org.verapdf.pd.PDDocument;
-import org.verapdf.pd.PDExtGState;
-import org.verapdf.pd.PDPage;
-import org.verapdf.pd.PDResources;
-import org.verapdf.pd.PDStructElem;
-import org.verapdf.pd.PDStructTreeNode;
+import org.verapdf.pd.*;
 import org.verapdf.pd.font.PDFont;
 import org.verapdf.pd.font.PDType3Font;
 import org.verapdf.pd.images.PDXForm;
@@ -32,11 +13,21 @@ import org.verapdf.pd.patterns.PDPattern;
 import org.verapdf.pd.patterns.PDShadingPattern;
 import org.verapdf.pd.patterns.PDTilingPattern;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * @author Maksim Bezrukov
  */
 public class FileSpecificationKeysHelper {
 	private final static Logger LOGGER = Logger.getLogger(FileSpecificationKeysHelper.class.getCanonicalName());
+
+	private static Set<COSKey> visitedKeys = new HashSet<>();
 
 	public static void registerFileSpecificationKeys(COSDocument document) {
 		PDDocument pdDocument = document.getPDDocument();
@@ -60,6 +51,7 @@ public class FileSpecificationKeysHelper {
 		} catch (IOException e) {
 			LOGGER.log(Level.SEVERE, "Can not get list of pages", e);
 		}
+		visitedKeys.clear();
 	}
 
 	private static void processStructElements(PDStructTreeNode structureNode) {
@@ -87,7 +79,7 @@ public class FileSpecificationKeysHelper {
 	}
 
 	private static void processXObject(PDXObject xObject) {
-		if (xObject != null) {
+		if (xObject != null && !isKeyVisited(xObject.getObject().getKey())) {
 			registerDictionaryAFKeys(xObject.getObject());
 			if (ASAtom.FORM.equals(xObject.getType())) {
 				parseResources(((PDXForm) xObject).getResources());
@@ -143,20 +135,21 @@ public class FileSpecificationKeysHelper {
 	}
 
 	private static void processExtGState(PDExtGState extGState) {
-		if (extGState == null) {
-			return;
+		if (extGState != null && !isKeyVisited(extGState.getObject().getKey())) {
+			processFont(extGState.getFont());
 		}
-		processFont(extGState.getFont());
 	}
 
 	private static void processFont(PDFont font) {
-		if (font != null && ASAtom.TYPE3.equals(font.getSubtype())) {
+		if (font != null
+				&& !isKeyVisited(font.getObject().getKey())
+				&& ASAtom.TYPE3.equals(font.getSubtype())) {
 			parseResources(((PDType3Font) font).getResources());
 		}
 	}
 
 	private static void processPattern(PDPattern pattern) {
-		if (pattern != null) {
+		if (pattern != null && !isKeyVisited(pattern.getObject().getKey())) {
 			if (pattern.getPatternType() == 1) {
 				parseResources(((PDTilingPattern) pattern).getResources());
 			} else if (pattern.getPatternType() == 2) {
@@ -190,7 +183,6 @@ public class FileSpecificationKeysHelper {
 
 	private static void parseResourcesXObjects(PDResources resources) {
 		for (ASAtom name : resources.getXObjectNames()) {
-
 			PDXObject xObject = resources.getXObject(name);
 			processXObject(xObject);
 
@@ -211,5 +203,13 @@ public class FileSpecificationKeysHelper {
 				StaticContainers.fileSpecificationKeys.add(key);
 			}
 		}
+	}
+
+	private static boolean isKeyVisited(COSKey key) {
+		if (visitedKeys.contains(key)) {
+			return true;
+		}
+		visitedKeys.add(key);
+		return false;
 	}
 }
