f2729dff5555f94072ef8921d1a6cc5e54828225
shem-sergey <sergey.shemyakov@duallab.com>
2017-06-29 15:48:35 +0300

    Fixed groupCS link getter in PDPage, removed link to cs from PDGroup.

--- validation-model/src/main/java/org/verapdf/gf/model/impl/pd/GFPDGroup.java
+++ validation-model/src/main/java/org/verapdf/gf/model/impl/pd/GFPDGroup.java
@@ -21,14 +21,7 @@
 package org.verapdf.gf.model.impl.pd;
 
 import org.verapdf.as.ASAtom;
-import org.verapdf.gf.model.factory.colors.ColorSpaceFactory;
-import org.verapdf.model.baselayer.Object;
-import org.verapdf.model.pdlayer.PDColorSpace;
 import org.verapdf.model.pdlayer.PDGroup;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * @author Maksim Bezrukov
@@ -36,8 +29,6 @@ import java.util.List;
 public class GFPDGroup extends GFPDObject implements PDGroup {
 
 	public static final String GROUP_TYPE = "PDGroup";
-
-	public static final String COLOR_SPACE = "colorSpace";
 
 	public GFPDGroup(org.verapdf.pd.PDGroup simplePDObject) {
 		super(simplePDObject, GROUP_TYPE);
@@ -49,23 +40,4 @@ public class GFPDGroup extends GFPDObject implements PDGroup {
 		return subtype == null ? null : subtype.getValue();
 	}
 
-	@Override
-	public List<? extends Object> getLinkedObjects(String link) {
-		if (COLOR_SPACE.equals(link)) {
-			return this.getColorSpace();
-		}
-		return super.getLinkedObjects(link);
-	}
-
-	private List<PDColorSpace> getColorSpace() {
-		org.verapdf.pd.colors.PDColorSpace pbColorSpace =
-				((org.verapdf.pd.PDGroup) this.simplePDObject).getColorSpace();
-		if (pbColorSpace != null) {
-			PDColorSpace colorSpace = ColorSpaceFactory.getColorSpace(pbColorSpace);
-			List<PDColorSpace> colorSpaces = new ArrayList<>(MAX_NUMBER_OF_ELEMENTS);
-			colorSpaces.add(colorSpace);
-			return Collections.unmodifiableList(colorSpaces);
-		}
-		return Collections.emptyList();
-	}
 }
