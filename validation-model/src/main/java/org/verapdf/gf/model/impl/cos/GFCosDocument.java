099b49210c0e4ad98ff5e36ead83bff7677cc518
MaximPlusov <36183885+MaximPlusov@users.noreply.github.com>
2020-01-08 11:00:45 +0300

    Code refactoring
    Code refactoring

--- validation-model/src/main/java/org/verapdf/gf/model/impl/cos/GFCosDocument.java
+++ validation-model/src/main/java/org/verapdf/gf/model/impl/cos/GFCosDocument.java
@@ -189,10 +189,10 @@ public class GFCosDocument extends GFCosObject implements CosDocument {
 	private static String getTrailerID(COSObject ids) {
 		if (ids != null && ids.getType() == COSObjType.COS_ARRAY) {
 			COSArray idArray = (COSArray) ids.getDirectBase();
-			StringBuilder builder = new StringBuilder();
 			if (idArray.size() != 2) {
-				LOGGER.log(Level.WARNING,"Value of ID is not an array of two byte strings");
+				LOGGER.log(Level.WARNING, "Value of ID is not an array of two byte strings");
 			}
+			StringBuilder builder = new StringBuilder();
 			for (COSObject id : idArray) {
 				if (id.getType() == COSObjType.COS_STRING) {
 					for (byte aByte : ((COSString) id.getDirectBase()).get()) {
