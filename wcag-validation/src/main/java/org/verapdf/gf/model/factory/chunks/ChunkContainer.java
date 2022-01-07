5d769c952d8d0cf7a9a2feb2457b60357c586593
MaximPlusov <maxplusim@mail.ru>
2022-01-07 23:08:49 +0300

    WCAG validation. Add curves parsing

--- wcag-validation/src/main/java/org/verapdf/gf/model/factory/chunks/ChunkContainer.java
+++ wcag-validation/src/main/java/org/verapdf/gf/model/factory/chunks/ChunkContainer.java
@@ -1,3 +1,23 @@
+/**
+ * This file is part of veraPDF Validation, a module of the veraPDF project.
+ * Copyright (c) 2015, veraPDF Consortium <info@verapdf.org>
+ * All rights reserved.
+ *
+ * veraPDF Validation is free software: you can redistribute it and/or modify
+ * it under the terms of either:
+ *
+ * The GNU General public license GPLv3+.
+ * You should have received a copy of the GNU General Public License
+ * along with veraPDF Validation as the LICENSE.GPL file in the root of the source
+ * tree.  If not, see http://www.gnu.org/licenses/ or
+ * https://www.gnu.org/licenses/gpl-3.0.en.html.
+ *
+ * The Mozilla Public License MPLv2+.
+ * You should have received a copy of the Mozilla Public License along with
+ * veraPDF Validation as the LICENSE.MPL file in the root of the source tree.
+ * If a copy of the MPL was not distributed with this file, you can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ */
 package org.verapdf.gf.model.factory.chunks;
 
 import org.verapdf.cos.COSKey;
@@ -11,6 +31,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Collections;
 
+/**
+ * @author Maxim Plushchov
+ */
 public class ChunkContainer {
 
 	private final Map<COSKey, Map<Long, List<IChunk>>> chunks;
