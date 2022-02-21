e061331153f3149363b47b885c557280a8ad5337
Angelina Pavlovets <angelinapavlovets@gmail.com>
2022-02-21 19:25:32 +0300

    Add converting fill color to RGB in case 'scn' and 'sc' operators

--- validation-model/src/main/java/org/verapdf/gf/model/impl/pd/functions/GFPDType0Function.java
+++ validation-model/src/main/java/org/verapdf/gf/model/impl/pd/functions/GFPDType0Function.java
@@ -21,14 +21,13 @@
 package org.verapdf.gf.model.impl.pd.functions;
 
 import org.verapdf.model.pdlayer.PDType0Function;
-import org.verapdf.pd.function.PDFunction;
 
 
 public class GFPDType0Function extends GFPDFunction implements PDType0Function {
 
     public static final String PD_TYPE0_FUNCTION_TYPE = "PDType0Function";
 
-    public GFPDType0Function(PDFunction function) {
+    public GFPDType0Function(org.verapdf.pd.function.PDType0Function function) {
         super(function, PD_TYPE0_FUNCTION_TYPE);
     }
 
--- validation-model/src/main/java/org/verapdf/gf/model/impl/pd/functions/GFPDType2Function.java
+++ validation-model/src/main/java/org/verapdf/gf/model/impl/pd/functions/GFPDType2Function.java
@@ -21,14 +21,13 @@
 package org.verapdf.gf.model.impl.pd.functions;
 
 import org.verapdf.model.pdlayer.PDType2Function;
-import org.verapdf.pd.function.PDFunction;
 
 
 public class GFPDType2Function extends GFPDFunction implements PDType2Function {
 
     public static final String PD_TYPE2_FUNCTION_TYPE = "PDType2Function";
 
-    public GFPDType2Function(PDFunction function) {
+    public GFPDType2Function(org.verapdf.pd.function.PDType2Function function) {
         super(function, PD_TYPE2_FUNCTION_TYPE);
     }
 
