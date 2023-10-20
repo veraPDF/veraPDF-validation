e0d33d849c86d04f63b3682cf78bc464840fe9a0
Maxim <maxplusim@mail.ru>
2023-10-20 14:42:50 +0300

    PDF/UA-2. Add new methods. Remove unnecessary methods

--- validation-model/src/main/java/org/verapdf/gf/model/impl/pd/gfse/GFSEFENote.java
+++ validation-model/src/main/java/org/verapdf/gf/model/impl/pd/gfse/GFSEFENote.java
@@ -20,15 +20,54 @@
  */
 package org.verapdf.gf.model.impl.pd.gfse;
 
+import org.verapdf.cos.COSArray;
+import org.verapdf.cos.COSKey;
+import org.verapdf.cos.COSObjType;
+import org.verapdf.cos.COSObject;
+import org.verapdf.gf.model.impl.containers.StaticContainers;
 import org.verapdf.gf.model.impl.pd.GFPDStructElem;
 import org.verapdf.model.selayer.SEFENote;
 import org.verapdf.pd.structure.PDStructElem;
 import org.verapdf.tools.AttributeHelper;
 import org.verapdf.tools.TaggedPDFConstants;
 
+import java.util.*;
+
 public class GFSEFENote extends GFPDStructElem implements SEFENote {
 
     public static final String FE_NOTE_STRUCTURE_ELEMENT_TYPE = "SEFENote";
+
+    @Override
+    public String getorphanRefs() {
+        COSKey key = simpleCOSObject.getKey();
+        if (key == null) {
+            return null;
+        }
+        Set<COSKey> set = StaticContainers.getStructElementsRefs().get(key);
+        if (set == null) {
+            return null;
+        }
+        COSObject ref = ((PDStructElem)simplePDObject).getRef();
+        Set<COSKey> refsKeys = new HashSet<>();
+        if (ref.getType() == COSObjType.COS_ARRAY) {
+            for (COSObject elem : (COSArray) ref.getDirectBase()) {
+                COSKey elementKey = elem.getKey();
+                if (elementKey != null) {
+                    refsKeys.add(elementKey);
+                }
+            }
+        }
+        Set<String> keys = new HashSet<>();
+        for (COSKey refKey : set) {
+            if (!refsKeys.contains(refKey)) {
+                keys.add(refKey.toString());
+            }
+        }
+        if (!keys.isEmpty()) {
+            return String.join(",", keys);
+        }
+        return null;
+    }
 
     public GFSEFENote(PDStructElem structElemDictionary) {
         super(structElemDictionary, TaggedPDFConstants.FENOTE, FE_NOTE_STRUCTURE_ELEMENT_TYPE);
