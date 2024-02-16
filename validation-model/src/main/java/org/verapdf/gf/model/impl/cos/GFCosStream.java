8e74631fb44cd46d78f7ae4ca05abd6afa4a619a
Maxim <maxplusim@mail.ru>
2024-02-16 12:31:34 +0300

    Fix type of GFSEMathMLStructElem

--- validation-model/src/main/java/org/verapdf/gf/model/impl/cos/GFCosStream.java
+++ validation-model/src/main/java/org/verapdf/gf/model/impl/cos/GFCosStream.java
@@ -28,7 +28,6 @@ import org.verapdf.model.coslayer.CosStream;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
