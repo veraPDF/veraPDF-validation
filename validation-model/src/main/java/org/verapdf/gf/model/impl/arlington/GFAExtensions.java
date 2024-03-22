f200e2e289eda251275c5219da381b945a53c86b
Maxim <maxplusim@mail.ru>
2024-03-22 21:21:46 +0300

    Arlington. Add GFAISO_DevExtensions

--- validation-model/src/main/java/org/verapdf/gf/model/impl/arlington/GFAExtensions.java
+++ validation-model/src/main/java/org/verapdf/gf/model/impl/arlington/GFAExtensions.java
@@ -21,6 +21,8 @@ public class GFAExtensions extends GFAObject implements AExtensions {
 		switch (link) {
 			case "Entries":
 				return getEntries();
+			case "ISO_":
+				return getISO_();
 			default:
 				return super.getLinkedObjects(link);
 		}
@@ -39,13 +41,89 @@ public class GFAExtensions extends GFAObject implements AExtensions {
 	private List<AExtensionsEntry> getEntries1_7() {
 		List<AExtensionsEntry> list = new LinkedList<>();
 		for (ASAtom key : baseObject.getKeySet()) {
-			if ("Type".equals(key.getValue())) {
+			if ("ISO_".equals(key.getValue()) || "Type".equals(key.getValue())) {
 				continue;
 			}
 			COSObject object = this.baseObject.getKey(key);
 			list.add(new GFAExtensionsEntry(object != null ? object.get() : null, this.baseObject, this.parentObject, keyName, key.getValue()));
 		}
 		return Collections.unmodifiableList(list);
+	}
+
+	private List<org.verapdf.model.baselayer.Object> getISO_() {
+		switch (StaticContainers.getFlavour()) {
+			case ARLINGTON1_7:
+				return getISO_1_7();
+			case ARLINGTON2_0:
+				return getISO_2_0();
+			default:
+				return Collections.emptyList();
+		}
+	}
+
+	private List<org.verapdf.model.baselayer.Object> getISO_1_7() {
+		COSObject object = getISO_Value();
+		if (object == null) {
+			return Collections.emptyList();
+		}
+		if (object.getType() == COSObjType.COS_DICT) {
+			List<AISO_DevExtensions> list = new ArrayList<>(1);
+			list.add(new GFAISO_DevExtensions((COSDictionary)object.getDirectBase(), this.baseObject, "ISO_"));
+			return Collections.unmodifiableList(list);
+		}
+		return Collections.emptyList();
+	}
+
+	private List<org.verapdf.model.baselayer.Object> getISO_2_0() {
+		COSObject object = getISO_Value();
+		if (object == null) {
+			return Collections.emptyList();
+		}
+		if (object.getType() == COSObjType.COS_ARRAY) {
+			List<AArrayOfDevExtensions> list = new ArrayList<>(1);
+			list.add(new GFAArrayOfDevExtensions((COSArray)object.getDirectBase(), this.baseObject, "ISO_"));
+			return Collections.unmodifiableList(list);
+		}
+		if (object.getType() == COSObjType.COS_DICT) {
+			List<AISO_DevExtensions> list = new ArrayList<>(1);
+			list.add(new GFAISO_DevExtensions((COSDictionary)object.getDirectBase(), this.baseObject, "ISO_"));
+			return Collections.unmodifiableList(list);
+		}
+		return Collections.emptyList();
+	}
+
+	@Override
+	public Boolean getcontainsISO_() {
+		return this.baseObject.knownKey(ASAtom.getASAtom("ISO_"));
+	}
+
+	public COSObject getISO_Value() {
+		COSObject object = this.baseObject.getKey(ASAtom.getASAtom("ISO_"));
+		return object;
+	}
+
+	@Override
+	public Boolean getisISO_Indirect() {
+		COSObject ISO_ = getISO_Value();
+		return getisIndirect(ISO_);
+	}
+
+	@Override
+	public String getISO_Type() {
+		COSObject ISO_ = getISO_Value();
+		return getObjectType(ISO_);
+	}
+
+	@Override
+	public Boolean getISO_HasTypeArray() {
+		COSObject ISO_ = getISO_Value();
+		return getHasTypeArray(ISO_);
+	}
+
+	@Override
+	public Boolean getISO_HasTypeDictionary() {
+		COSObject ISO_ = getISO_Value();
+		return getHasTypeDictionary(ISO_);
 	}
 
 	@Override
