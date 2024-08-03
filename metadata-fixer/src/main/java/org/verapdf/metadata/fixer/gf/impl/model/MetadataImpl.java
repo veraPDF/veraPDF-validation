f1f3fd8c67003f79e174453e3a387c48f6ce17a4
Maxim <maxplusim@mail.ru>
2024-08-03 19:54:42 +0300

    Update license headers

--- metadata-fixer/src/main/java/org/verapdf/metadata/fixer/gf/impl/model/MetadataImpl.java
+++ metadata-fixer/src/main/java/org/verapdf/metadata/fixer/gf/impl/model/MetadataImpl.java
@@ -1,6 +1,6 @@
 /**
  * This file is part of veraPDF Metadata Fixer, a module of the veraPDF project.
- * Copyright (c) 2015, veraPDF Consortium <info@verapdf.org>
+ * Copyright (c) 2015-2024, veraPDF Consortium <info@verapdf.org>
  * All rights reserved.
  *
  * veraPDF Metadata Fixer is free software: you can redistribute it and/or modify
