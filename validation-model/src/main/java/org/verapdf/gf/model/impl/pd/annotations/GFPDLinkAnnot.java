c7e7ee6f466e6707f5341634714bd166c7805c97
Maxim <maxplusim@mail.ru>
2025-07-11 13:27:34 +0300

    Fix NullPointerException in GFPDLinkAnnot

--- validation-model/src/main/java/org/verapdf/gf/model/impl/pd/annotations/GFPDLinkAnnot.java
+++ validation-model/src/main/java/org/verapdf/gf/model/impl/pd/annotations/GFPDLinkAnnot.java
@@ -116,7 +116,7 @@ public class GFPDLinkAnnot extends GFPDAnnot implements PDLinkAnnot {
 		COSObject destination = simpleCOSObject;
 		if (simpleCOSObject.knownKey(ASAtom.A)) {
 			PDAction action = ((PDAnnotation) simplePDObject).getA();
-			if (ASAtom.GO_TO.equals(action.getSubtype())) {
+			if (action != null && ASAtom.GO_TO.equals(action.getSubtype())) {
 				destination = action.knownKey(ASAtom.SD) ? action.getStructureDestination() : action.getDestination();
 			}
 		} else if (simpleCOSObject.knownKey(ASAtom.DEST)) {
