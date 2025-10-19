96a3349d1e8c2be6fd2582628ce1b27d20a8779f
Maxim <maxplusim@mail.ru>
2025-10-19 23:36:32 +0300

    Arlington. Add methods for Next entries in actions

--- validation-model/src/main/java/org/verapdf/gf/model/impl/arlington/GFAActionNOP.java
+++ validation-model/src/main/java/org/verapdf/gf/model/impl/arlington/GFAActionNOP.java
@@ -17,6 +17,120 @@ public class GFAActionNOP extends GFAObject implements AActionNOP {
 	}
 
 	@Override
+	public List<? extends org.verapdf.model.baselayer.Object> getLinkedObjects(String link) {
+		switch (link) {
+			case "Next":
+				return getNext();
+			default:
+				return super.getLinkedObjects(link);
+		}
+	}
+
+	private List<org.verapdf.model.baselayer.Object> getNext() {
+		switch (StaticContainers.getFlavour()) {
+			case ARLINGTON1_2:
+			case ARLINGTON1_3:
+			case ARLINGTON1_4:
+			case ARLINGTON1_5:
+			case ARLINGTON1_6:
+			case ARLINGTON1_7:
+			case ARLINGTON2_0:
+				return getNext1_2();
+			default:
+				return Collections.emptyList();
+		}
+	}
+
+	private List<org.verapdf.model.baselayer.Object> getNext1_2() {
+		COSObject object = getNextValue();
+		if (object == null) {
+			return Collections.emptyList();
+		}
+		if (object.getType() == COSObjType.COS_ARRAY) {
+			List<AArrayOfActions> list = new ArrayList<>(1);
+			list.add(new GFAArrayOfActions((COSArray)object.getDirectBase(), this.baseObject, "Next"));
+			return Collections.unmodifiableList(list);
+		}
+		if (object.getType() == COSObjType.COS_DICT) {
+			org.verapdf.model.baselayer.Object result = getNextDictionary1_2(object.getDirectBase(), "Next");
+			List<org.verapdf.model.baselayer.Object> list = new ArrayList<>(1);
+			if (result != null) {
+				list.add(result);
+			}
+			return Collections.unmodifiableList(list);
+		}
+		return Collections.emptyList();
+	}
+
+	private org.verapdf.model.baselayer.Object getNextDictionary1_2(COSBase base, String keyName) {
+		COSObject subtype = base.getKey(ASAtom.getASAtom("S"));
+		String subtypeValue = subtype != null ? subtype.getString() : null;
+		if (subtypeValue == null) {
+			return null;
+		}
+		switch (subtypeValue) {
+			case "GoTo":
+				return new GFAActionGoTo(base, this.baseObject, keyName);
+			case "GoToR":
+				return new GFAActionGoToR(base, this.baseObject, keyName);
+			case "Hide":
+				return new GFAActionHide(base, this.baseObject, keyName);
+			case "ImportData":
+				return new GFAActionImportData(base, this.baseObject, keyName);
+			case "Launch":
+				return new GFAActionLaunch(base, this.baseObject, keyName);
+			case "Movie":
+				return new GFAActionMovie(base, this.baseObject, keyName);
+			case "NOP":
+				return new GFAActionNOP(base, this.baseObject, keyName);
+			case "Named":
+				return new GFAActionNamed(base, this.baseObject, keyName);
+			case "ResetForm":
+				return new GFAActionResetForm(base, this.baseObject, keyName);
+			case "SetState":
+				return new GFAActionSetState(base, this.baseObject, keyName);
+			case "Sound":
+				return new GFAActionSound(base, this.baseObject, keyName);
+			case "SubmitForm":
+				return new GFAActionSubmitForm(base, this.baseObject, keyName);
+			case "Thread":
+				return new GFAActionThread(base, this.baseObject, keyName);
+			case "URI":
+				return new GFAActionURI(base, this.baseObject, keyName);
+			default:
+				return null;
+		}
+	}
+
+	@Override
+	public Boolean getcontainsNext() {
+		return this.baseObject.knownKey(ASAtom.getASAtom("Next"));
+	}
+
+	public COSObject getNextValue() {
+		COSObject object = this.baseObject.getKey(ASAtom.getASAtom("Next"));
+		return object;
+	}
+
+	@Override
+	public String getNextType() {
+		COSObject Next = getNextValue();
+		return getObjectType(Next);
+	}
+
+	@Override
+	public Boolean getNextHasTypeArray() {
+		COSObject Next = getNextValue();
+		return getHasTypeArray(Next);
+	}
+
+	@Override
+	public Boolean getNextHasTypeDictionary() {
+		COSObject Next = getNextValue();
+		return getHasTypeDictionary(Next);
+	}
+
+	@Override
 	public Boolean getcontainsS() {
 		return this.baseObject.knownKey(ASAtom.getASAtom("S"));
 	}
